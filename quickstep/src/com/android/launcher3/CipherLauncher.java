46db98f373ad3a2353b79dbbb2910c5245d94cd0
Aryan Sinha <sinha.aryan03@gmail.com>
2022-04-09 08:45:20 +0000

    Launcher3: Refactor to Cipher Launcher * Refactor Custom Launcher to Cipher Launcher * Update Launcher icons * Refactor App Name
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- quickstep/src/com/android/launcher3/CipherLauncher.java
+++ quickstep/src/com/android/launcher3/CipherLauncher.java
@@ -18,6 +18,6 @@ package com.android.launcher3;
 
 import com.android.launcher3.uioverrides.QuickstepLauncher;
 
-public class CustomLauncher extends QuickstepLauncher {
+public class CipherLauncher extends QuickstepLauncher {
 
 }
